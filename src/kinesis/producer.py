5081d05d63dd847e107a1e140377ef3c4e46fffd
Borgstrom ♕ <evan@borgstrom.ca>
2017-07-26 08:39:52 -0700

    Send at most 1000 messages per buffer cycle (#4)

--- src/kinesis/producer.py
+++ src/kinesis/producer.py
@@ -13,8 +13,15 @@ log = logging.getLogger(__name__)
 
 class AsyncProducer(SubprocessLoop):
     """Async accumulator and producer based on a multiprocessing Queue"""
-    MAX_SIZE = 2 ** 20
+    # Tell our subprocess loop that we don't want to terminate on shutdown since we want to drain our queue first
     TERMINATE_ON_SHUTDOWN = False
+
+    # This it the max size data that we'll send in a single call.  We use 99% of 1Mb to account for the extra overhead
+    # of JSON syntax that is not taken into account when we use sys.getsizeof since it's measuring the python object
+    MAX_SIZE = int((2 ** 20) * .99)
+
+    # This is the max number of messages that we'll send in a single call.
+    MAX_COUNT = 1000
 
     def __init__(self, stream_name, buffer_time, queue, boto3_session=None):
         self.stream_name = stream_name
@@ -32,11 +39,16 @@ class AsyncProducer(SubprocessLoop):
 
     def loop(self):
         records_size = 0
+        records_count = 0
         timer_start = time.time()
 
         while self.alive and (time.time() - timer_start) < self.buffer_time:
+            # we want our queue to block up until the end of this buffer cycle, so we set out timeout to the amount
+            # remaining in buffer_time by substracting how long we spent so far during this cycle
+            queue_timeout = self.buffer_time - (time.time() - timer_start)
             try:
-                data = self.queue.get(block=True, timeout=0.25)
+                log.debug("Fetching from queue with timeout: %s", queue_timeout)
+                data = self.queue.get(block=True, timeout=queue_timeout)
             except Queue.Empty:
                 continue
 
@@ -47,12 +59,17 @@ class AsyncProducer(SubprocessLoop):
 
             records_size += sys.getsizeof(record)
             if records_size >= self.MAX_SIZE:
-                log.debug("Records full!  Adding to next_records: %s", record)
+                log.debug("Records exceed MAX_SIZE!  Adding to next_records: %s", record)
                 self.next_records = [record]
                 break
 
             log.debug("Adding to records (%d bytes): %s", records_size, record)
             self.records.append(record)
+
+            records_count += 1
+            if records_count == self.MAX_COUNT:
+                log.debug("Records have reached MAX_COUNT!  Flushing records.")
+                break
 
         self.flush_records()
 
